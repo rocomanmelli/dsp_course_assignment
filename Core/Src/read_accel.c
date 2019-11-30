ae77fab1a099ee7cc0a9c0d556799d98ed1e218d
Román Comelli <roman.comelli@gmail.com>
2019-11-30 16:22:51 -0300

    Add UART communication and transmition of an acceleration (tested with HC-05 Bluetooth module).

--- Core/Src/read_accel.c
+++ Core/Src/read_accel.c
@@ -1,7 +1,12 @@
 #include "cmsis_os2.h"
 #include "read_accel.h"
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 #include "stm32f401_discovery_accelerometer.h"
+
+static ACCELERO_DrvTypeDef *AccelerometerDrv;
+extern UART_HandleTypeDef huart2;
 
 
 void InitReadAccelTask()
@@ -11,15 +16,28 @@ void InitReadAccelTask()
 	 * the I2C is busy. In this case, a power off - power on
 	 * must be performed.*/
     BSP_ACCELERO_Init();
+    AccelerometerDrv = &Lsm303dlhcDrv;
 }
 
 
 void ReadAccelTask(void *argument)
 {
     int16_t pDataXYZ[3];
+    uint8_t new_data;
   for(;;)
   {
-    osDelay(1);
-    BSP_ACCELERO_GetXYZ(pDataXYZ);
+    new_data = COMPASSACCELERO_IO_Read(ACC_I2C_ADDRESS, LSM303DLHC_STATUS_REG_A);
+    if (new_data && 0x8){
+      BSP_ACCELERO_GetXYZ(pDataXYZ);
+      uint8_t hola[20];
+      itoa(pDataXYZ[0], hola, 10);
+      HAL_UART_Transmit(&huart2, hola, strlen(hola) + 1, HAL_UART_STATE_TIMEOUT);
+    }
+    else{
+      pDataXYZ[0] = 0;
+      pDataXYZ[1] = 0;
+      pDataXYZ[2] = 0;
+    }
+    
   }
 }
