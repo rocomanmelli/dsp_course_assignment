10dce2ce16d0f3dc769170ba1af5b5b293e166c4
Román Comelli <roman.comelli@gmail.com>
2019-12-14 09:51:53 -0300

    Change way of acquisition.

--- Core/Src/read_accel.c
+++ Core/Src/read_accel.c
@@ -8,6 +8,8 @@
 static ACCELERO_DrvTypeDef *AccelerometerDrv;
 extern UART_HandleTypeDef huart2;
 
+#define BUFFER_LENGTH 2000
+
 
 void InitReadAccelTask()
 {
@@ -23,21 +25,30 @@ void InitReadAccelTask()
 void ReadAccelTask(void *argument)
 {
     int16_t pDataXYZ[3];
+    int16_t data_z[BUFFER_LENGTH];
     uint8_t new_data;
-  for(;;)
-  {
-    new_data = COMPASSACCELERO_IO_Read(ACC_I2C_ADDRESS, LSM303DLHC_STATUS_REG_A);
-    if (new_data && 0x8){
-      BSP_ACCELERO_GetXYZ(pDataXYZ);
-      uint8_t hola[20];
-      itoa(pDataXYZ[0], hola, 10);
-      HAL_UART_Transmit(&huart2, hola, strlen(hola) + 1, HAL_UART_STATE_TIMEOUT);
+    uint16_t index;
+    for(;;)
+    {
+        /* Data acquisition. */
+        index = 0;
+        while (index < BUFFER_LENGTH)
+        {
+            new_data = COMPASSACCELERO_IO_Read(ACC_I2C_ADDRESS,
+                LSM303DLHC_STATUS_REG_A);
+            if (new_data && 0x8)
+            {
+                BSP_ACCELERO_GetXYZ(pDataXYZ);
+                data_z[index] = pDataXYZ[2];
+                index++;
+            }
+        }
+
+        /* Send data through UART. */
+        HAL_UART_Transmit(&huart2, (uint8_t *) data_z, BUFFER_LENGTH * 2,
+            HAL_UART_STATE_TIMEOUT);
+        
+        /* Process data. */
+        
     }
-    else{
-      pDataXYZ[0] = 0;
-      pDataXYZ[1] = 0;
-      pDataXYZ[2] = 0;
-    }
-    
-  }
 }
